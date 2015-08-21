42783334634fac36b277b854cbb22380c720aa25
Alfonz <m.alfonz@gmail.com>
2015-08-21 15:44:45 +0800

    Update Reg

--- source/Reg.hx
+++ source/Reg.hx
@@ -2,36 +2,25 @@ package;
 
 import flixel.util.FlxSave;
 
-/**
- * Handy, pre-built Registry class that can be used to store 
- * references to objects and other things for quick-access. Feel
- * free to simply ignore it or change it in any way you like.
- */
 class Reg
 {
-	/**
-	 * Generic levels Array that can be used for cross-state stuff.
-	 * Example usage: Storing the levels of a platformer.
-	 */
+	// Assets
+	public static inline var PLAYER_SPRITESHEET = "assets/images/player_anim.png";
+	public static inline var ENEMY1_SPRITESHEET = "assets/images/enemy_anim.png";
+
+	// Tile Size
+	public static var T_WIDTH:Int = 16;
+	public static var T_HEIGHT:Int = 16;
+
+	// Misc
 	public static var levels:Array<Dynamic> = [];
-	/**
-	 * Generic level variable that can be used for cross-state stuff.
-	 * Example usage: Storing the current level number.
-	 */
 	public static var level:Int = 0;
-	/**
-	 * Generic scores Array that can be used for cross-state stuff.
-	 * Example usage: Storing the scores for level.
-	 */
 	public static var scores:Array<Dynamic> = [];
-	/**
-	 * Generic score variable that can be used for cross-state stuff.
-	 * Example usage: Storing the current score.
-	 */
 	public static var score:Int = 0;
-	/**
-	 * Generic bucket for storing different FlxSaves.
-	 * Especially useful for setting up multiple save slots.
-	 */
 	public static var saves:Array<FlxSave> = [];
+
+	public static function getPlayerAnim(Player:FlxSprite){
+		Player.loadGraphic(PLAYER_SPRITESHEET, true, 8);
+		Player.animation.add("test", [1], 30);
+	}
 }
