a8871cf068936a6d062a307efc3eec585fb5238c
Alfonz <m.alfonz@gmail.com>
2015-08-21 16:15:42 +0800

    Update dump

--- source/Dump.hx
+++ source/Dump.hx
@@ -5,18 +5,21 @@
  */
 
 // variable
-var gibs:FlxEmitter;
+public var gibs:FlxEmitter;
 
 // initialize
-gibs = new FlxEmitter();
-gibs.setXSpeed( -100, 100);
-gibs.setYSpeed( -150, 0);
-gibs.setRotation( -360, 0);
-gibs.gravity = 350;
-gibs.bounce = 0.3;
-gibs.makeParticles("assets/images/enemy_gibs.png", 100, 10, true, 0.5);
+setupGibs(); // inside create function
 
+public function setupGibs(){
+	gibs = new FlxEmitter();
+	gibs.setXSpeed( -100, 100);
+	gibs.setYSpeed( -150, 0);
+	gibs.setRotation( -360, 0);
+	gibs.gravity = 350;
+	gibs.bounce = 0.3;
+	gibs.makeParticles("assets/images/enemy_gibs.png", 100, 10, true, 0.5);
+}
 
-// to start particle
-gibs.at(FlxObject); // automatic get FlxObject's position
+// to play emitter
+gibs.at(new FlxObject(x,y)); // where x y = position
 gibs.start(true,1,0,20,3);	// http://api.haxeflixel.com/flixel/effects/particles/FlxTypedEmitter.html
